c1dcd3ff2af01a289eef4dd34328ed51149248fd
Swen Loeffler <swen.loeffler@erne.net>
2025-01-21 18:59:04 +0100

    update docker compose

--- src/services/db.ts
+++ src/services/db.ts
@@ -3,7 +3,8 @@ import { neo4jConfig } from "../config/neo4jConfig";
 
 const driver = neo4j.driver(
   `${neo4jConfig.protocol}://${neo4jConfig.host}:${neo4jConfig.port}`,
-  neo4j.auth.basic(neo4jConfig.dbUser, neo4jConfig.dbPassword)
+  neo4j.auth.basic(neo4jConfig.dbUser, neo4jConfig.dbPassword),
+  { encrypted: false}
 );
 
 export const session = driver.session();
